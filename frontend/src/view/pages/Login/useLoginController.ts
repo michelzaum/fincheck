07a8075dbe4d389ea684c075a839c582df255d08
Michel Oliveira <michel.oliveira999@hotmail.com>
2024-02-15 22:22:18 -0300

    feat: Showing a spinner when application is loading

--- frontend/src/view/pages/Login/useLoginController.ts
+++ frontend/src/view/pages/Login/useLoginController.ts
@@ -1,7 +1,10 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { httpClient } from '../../../app/services/httpClient';
+import toast from 'react-hot-toast';
+import { useMutation } from '@tanstack/react-query';
+import { SigninParams } from '../../../app/services/authService/signin';
+import { authService } from '../../../app/services/authService';
 
 const schema = z.object({
   email: z.string()
@@ -16,15 +19,25 @@ type FormData = z.infer<typeof schema>;
 export function useLoginController() {
   const {
     register,
-    handleSubmit: hookFormHandleSubmit,
+    handleSubmit: hookFormSubmit,
     formState: { errors },
   } = useForm<FormData>({
     resolver: zodResolver(schema)
   });
 
-  const handleSubmit = hookFormHandleSubmit(async (data) => {
-    await httpClient.post('/auth/signin', data);
+  const { mutateAsync, isLoading } = useMutation({
+    mutationFn: async (data: SigninParams) => {
+      return authService.signin(data);
+    },
   });
 
-  return { handleSubmit, register, errors };
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try {
+      await mutateAsync(data);
+    } catch {
+      toast.error('Credenciais inválidas!');
+    }
+  });
+
+  return { handleSubmit, register, errors, isLoading };
 }
