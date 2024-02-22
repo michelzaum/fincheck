7260db8efb54b0b1f5ef298925911c5838242a80
Michel Oliveira <michel.oliveira999@hotmail.com>
2024-02-22 00:05:50 -0300

    feat: Creating an interceptor to handle with invalid access token

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,9 +1,13 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useEffect, useState } from 'react';
 import { localStorageKeys } from '../config/localStorageKeys';
+import { useQuery } from '@tanstack/react-query';
+import { userService } from '../services/usersService';
+import toast from 'react-hot-toast';
 
 interface AuthContextValue {
   signedIn: boolean;
   signin(accessToken: string): void;
+  signout(): void;
 }
 
 export const AuthContext = createContext<AuthContextValue>({} as AuthContextValue);
@@ -15,14 +19,33 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     return !!storedAccessToken;
   });
 
+  const { isError } = useQuery({
+    queryKey: ['users', 'me'],
+    queryFn: () =>  userService.me(),
+    enabled: signedIn,
+  });
+
   const signin = useCallback((accessToken: string) => {
     localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
 
     setSignedIn(true);
   }, []);
 
+  const signout = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+
+    setSignedIn(false);
+  }, []);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error('Sua sessão expirou')
+      signout();
+    }
+  }, [isError, signout]);
+
   return (
-    <AuthContext.Provider value={{ signedIn, signin }}>
+    <AuthContext.Provider value={{ signedIn, signin, signout }}>
       {children}
     </AuthContext.Provider>
   )
