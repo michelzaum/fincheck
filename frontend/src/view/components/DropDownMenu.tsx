4442b56186da82d84dce4d28a47fa652c7455044
Michel Oliveira <michel.oliveira999@hotmail.com>
2024-03-14 21:21:39 -0300

    feat: Creating user logout dropdown menu

---
+++ frontend/src/view/components/DropDownMenu.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable react-refresh/only-export-components */
+import * as RadixDropDownMenu from '@radix-ui/react-dropdown-menu';
+import React from 'react';
+import { cn } from '../../app/utils/cn';
+
+function DropDownMenuRoot({ children }: { children: React.ReactNode }) {
+  return (
+    <RadixDropDownMenu.Root>
+      {children}
+    </RadixDropDownMenu.Root>
+  )
+}
+
+function DropDownMenuTrigger({ children }: { children: React.ReactNode }) {
+  return (
+    <RadixDropDownMenu.Trigger className='outline-none'>
+      {children}
+    </RadixDropDownMenu.Trigger>
+  )
+}
+
+interface DropDownMenuContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+function DropDownMenuContent({ children, className }: DropDownMenuContentProps) {
+  return (
+    <RadixDropDownMenu.Portal>
+      <RadixDropDownMenu.Content className={cn(
+        `rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] data-[side=bottom]:animate-slide-up-and-fade`,
+        className,
+      )}>
+        {children}
+      </RadixDropDownMenu.Content>
+    </RadixDropDownMenu.Portal>
+  )
+}
+
+interface DropDownMenuItemProps {
+  children: React.ReactNode;
+  className?: string;
+  onSelect?(): void;
+}
+
+function DropDownMenuItem({ children, className, onSelect }: DropDownMenuItemProps) {
+  return (
+    <RadixDropDownMenu.Item
+      onSelect={onSelect}
+      className={cn(
+      `min-h-12 outline-none flex items-center p-4 text-gray-800 text-sm data-[highlighted]:bg-gray-50
+      rounded-2xl transition-colors cursor-pointer`,
+      className,
+    )}
+    >
+      {children}
+    </RadixDropDownMenu.Item>
+  )
+}
+
+export const DropDownMenu = {
+  Root: DropDownMenuRoot,
+  Trigger: DropDownMenuTrigger,
+  Content: DropDownMenuContent,
+  Item: DropDownMenuItem,
+};
--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,7 +1,26 @@
+import { ExitIcon } from '@radix-ui/react-icons';
+import { DropDownMenu } from './DropDownMenu';
+import { useAuth } from '../../app/hooks/useAuth';
+
 export function UserMenu() {
+  const { signout } = useAuth();
   return (
-    <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">MO</span>
-    </div>
+    <DropDownMenu.Root>
+      <DropDownMenu.Trigger>
+        <div className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">MO</span>
+        </div>
+      </DropDownMenu.Trigger>
+
+      <DropDownMenu.Content className='w-32'>
+        <DropDownMenu.Item
+          className='flex items-center justify-between'
+          onSelect={signout}
+        >
+          Sair
+          <ExitIcon className='h-4 w-4' />
+        </DropDownMenu.Item>
+      </DropDownMenu.Content>
+    </DropDownMenu.Root>
   )
 }
